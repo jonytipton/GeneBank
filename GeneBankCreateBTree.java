a1a920b12cb493b1c53e392e01dfd5771b40d8eb
Samantha Maxey <SamanthaMaxey299@u.boisestate.edu>
2018-11-15 12:05:33 -0700

    Created GeneBankBTree.java

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -1,1 +1,14 @@
+import java.io.File;
 
+public class GeneBankCreateBTree {
+	
+	private int degree;
+	private int cacheSize;
+	private File file;
+	private int sequenceLength;
+	private int debugLevel;
+	
+	public static void main(String [] args) {
+		
+	}
+}
