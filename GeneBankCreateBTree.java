4c6300bba21ab090b2beb72c4e92e2105ed8d837
samanthamaxey299 <samanthamaxey299@onyxnode64.boisestate.edu>
2018-11-16 13:20:18 -0700

    Updated main class

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -1,17 +1,21 @@
+//GIT
+
 import java.io.File;
 
 public class GeneBankCreateBTree {
+	private static String degree;
+	private static String cacheSize;
+	public static boolean cacheInUse;
+	private static File file;
+	private static String sequenceLength;
+	private static String debugLevel;
 	
-	private int degree;
-	private int cacheSize;
-	public boolean cacheInUse;
-	private File file;
-	private int sequenceLength;
-	private int debugLevel;
-	
+	/**
+	 * Main class that sets the arguments into variables
+	 */
 	public static void main(String [] args) {
 		if (args.length == 6) {
-			if(args[0].equals(1)) {
+			if(args[0].equals("1")) {
 				cacheInUse = true;
 			}
 			else {
@@ -19,11 +23,24 @@ public class GeneBankCreateBTree {
 			}
 			degree = args[1];
 			file = new File(args[2]);
+			sequenceLength = args[3];
+			cacheSize = args[4];
+			debugLevel = args[5];
 			
+		}else if (args.length == 5 && args[0].equals("1")) {
+			cacheInUse = true;
+			degree = args[1];
+			file = new File(args[2]);
+			sequenceLength = args[3];
+			cacheSize = args[4];
+			degree = "0";
 			
-		}else if (args.length == 5 && args[0].equals(1)) {
-			
-		}else if (args.length == 5 && args[0].equals(0)) {
+		}else if (args.length == 5 && args[0].equals("0")) {
+			cacheInUse = false;
+			degree = args[1];
+			file = new File(args[2]);
+			sequenceLength = args[3];
+			degree = args[4];
 			
 		}else if (args.length < 5 || args.length > 6) {
 			System.out.println("Usage: java GeneBankCreateBTree" +
