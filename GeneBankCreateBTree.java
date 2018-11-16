14aef47f0ded8305be3037200869d9753dbd7d86
samanthamaxey299 <samanthamaxey299@onyxnode64.boisestate.edu>
2018-11-16 11:54:53 -0700

    Started on main for arguments -SM

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -9,6 +9,19 @@ public class GeneBankCreateBTree {
 	private int debugLevel;
 	
 	public static void main(String [] args) {
-		
+		if (args.length == 6) {
+			
+		}else if (args.length == 5 && args[1].equals(1)) {
+			
+		}else if (args.length == 5 && args[1].equals(0)) {
+			
+		}else if (args.length < 5 || args.length > 6) {
+			System.out.println("Usage: java GeneBankCreateBTree" +
+		" <0/1 (no/with Cache)> <degree> <gbk file> <sequence length>" +
+		" [<cache size>] [<debug level>].\n" +
+		"Message: Debug Level can be 0 or 1. 0 for regular output, 1 for" +
+		" creating a file named 'dump' that contains all DNA strings in the "
+		+ "sequence length you want.");
+		}
 	}
 }
