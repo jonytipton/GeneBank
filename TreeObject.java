03d6b00944c2a54195f410c97574475b38fc5a52
bridgettemilgie <bridgettemilgie@bridgettes-air.boisestate.edu>
2018-11-21 10:52:51 -0700

    tree object

--- TreeObject.java
+++ TreeObject.java
@@ -1,1 +1,43 @@
-
+public class TreeObject implements Comparable<TreeObject>{
+	
+	private int frequency;
+	private long data;
+	
+	public TreeObject(long data, int frequency){
+		this.data = data;
+		this.frequency = frequency;
+	}
+	
+	public TreeObject(long data){
+		this.data = data;
+		this.frequency = 1;
+	}
+	
+	public void increaseFrequency(){
+		this.frequency++;
+	}
+	
+	public int getFrequency(){
+		return frequency;
+	}
+	
+	public long getData(){
+		return data;
+	}
+	
+	public int compareTo(TreeObject newItem){
+		if(this.data < newItem){
+			return -1;
+		}else if(this.data > newItem){
+			return 1;
+		}else
+			return 0;
+	}
+	
+	public String toString(){
+		String s = new String();
+		s+= "Key: " + data + " frequency: " + frequency;
+		return s;
+	}
+	
+}
