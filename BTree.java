91a1c1f87c293f57164f575e5a6332a46384270d
bridgettemilgie <bridgettemilgie@192.168.0.5>
2018-11-26 12:54:24 -0700

    bTreeNode updated within BTree

--- BTree.java
+++ BTree.java
@@ -1,10 +1,156 @@
 import java.util.LinkedList;
 
-public class BTree
-{
-	
+public class BTree{
+
 	private int degree;
 	private BTreeNode root;
+	private int BTreeOffset;
+	private int nodeSize;
+	private int insertionPoint;
+	private File file;
+	private RandomAccessFile RAF;
+	
+	private class BTreeNode
+	{
+		private int parent;
+		private LinkedList<TreeObject> keys;
+		private LinkedList<Integer> children;
+		private int numKeys;
+		private int offset;
+		private boolean isLeaf;
+		
+		//not sure if we need this vars
+		private boolean isFull;
+		private int degree;
+		private boolean root;
+		//private int level; offset may be better to use based on the write up
+		
+		public BTreeNode()
+		{	
+			children = new LinkedList<Integer>();
+			keys = new LinkedList<TreeObject>();
+			numKeys = 0;
+			parent = -1;
+			//not sure if we need these three below
+			leaf = true;
+			isFull = false;
+			level = 0;
+		}
+		
+		public int getParent(){
+			return parent;
+		}
+		
+		public void setParent(int parent){
+			this.parent = parent;
+		}
+		
+		public long getKey(long findKey)
+		{
+			return;
+		}
+		
+		public int getOffset()
+		{
+			return offset;
+		}
+		
+		public void setOffset(int offset)
+		{
+			this.offset = offset;
+		}
+		
+		public TreeObject getKey(int i){
+			TreeObject obj = keys.get(i);
+			return obj;
+		}
+		
+		public void addKey(TreeObject o)
+		{
+			keys.add(o);
+		}
+		
+		public void addKey(TreeObject o, int i)
+		{
+			keys.add(i, o);
+		}
+		
+		public TreeObject removeKey(int i){
+			return keys.remove(i);
+		}
+		
+		public LinkedList<TreeObject> getKeys()
+		{
+			return keys;
+		}
+		
+		public int getNumKeys()
+		{
+			return numKeys;
+		}
+		
+		public void setNumKeys(int numKeys){
+			this.numKeys = numKeys;
+		}
+		
+		public boolean isRoot()
+		{
+			return root;
+		}
+		
+		public boolean isLeaf()
+		{
+			return isLeaf;
+		}
+		
+		public boolean isFull()
+		{
+			return isFull();
+		}
+		
+		public void setIsLeaf(boolean isLeaf)
+		{
+			this.isLeaf = isLeaf;
+		}
+		
+		public LinkedList<Integer> getChildren()
+		{
+			return children;
+		}
+		
+		public void addChild(int index)
+		{
+			children.add(index);
+		}
+		
+		public void addChild(Integer a, int index)
+		{
+			children.add(index, a);
+		}
+		
+		public int getChild(int index)
+		{
+			return children.get(index).intValue();
+		}
+		
+		public BTreeNode removeChild(int index)
+		{
+			return children.remove(index);
+		}
+		
+		public String toString(){
+			String s = new String();
+			s += "Keys: ";
+			for(int i = 0; i < keys.size(); i++){
+				s+= (keys.get(i) + " ");
+			}
+			s += "\nchildren: ";
+			for(int i = 0; i < children.size(); i++){
+				s += (children.get(i) + " " );
+			}
+			return s;
+		}
+	}
 	
 	public BTree(int degree)
 	{
@@ -46,96 +192,5 @@ public class BTree
 		return root;
 	}
 	
-	private class BTreeNode
-	{
-		private LinkedList<Long> key;
-		private int numKeys;
-		private boolean root;
-		private boolean leaf;
-		private boolean isFull;
-		private LinkedList<BTreeNode> children;
-		private int degree;
-		private int parent;
-		private int level;
-		
-		public BTreeNode()
-		{	
-			children = new LinkedList<BTreeNode>();
-			key = new LinkedList<Long>();
-			leaf = true;
-			isFull = false;
-			parent = -1;
-			level = 0;
-		}
-
-		public long getKey(long findKey)
-		{
-			return;
-		}
-		
-		public int getLevel()
-		{
-			return level;
-		}
-		
-		public void setLevel(int newLevel)
-		{
-			this.level = newLevel;
-		}
-		
-		public void addKey(long newKey)
-		{
-			key.add(newKey);
-		}
-		
-		public int getNumKeys()
-		{
-			return numKeys;
-		}
-		
-		public boolean isRoot()
-		{
-			return root;
-		}
-		
-		public boolean isLeaf()
-		{
-			return leaf;
-		}
-		
-		public boolean isFull()
-		{
-			return isFull();
-		}
-		
-		public void setIsLeaf(boolean isLeaf)
-		{
-			this.leaf = isLeaf;
-		}
-		
-		public LinkedList<Long> getKey()
-		{
-			return key;
-		}
-		
-		public LinkedList<BTreeNode> getChildren()
-		{
-			return children;
-		}
-		
-		public void addChild(BTreeNode child)
-		{
-			children.add(child);
-		}
-		
-		public BTreeNode getChild(int index)
-		{
-			return children.get(index);
-		}
-		
-		public BTreeNode removeChild(int index)
-		{
-			return children.remove(index);
-		}
-	}
+	
 }
