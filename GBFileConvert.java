1599262e879d4637a28ddd969c02372a432c5923
Jonathan Titpon <42556403+jonytipton@users.noreply.github.com>
2018-11-28 12:14:12 -0700

    GBFileConvert.java

---
+++ GBFileConvert.java
@@ -0,0 +1,92 @@
+
+public class GBFileConvert {
+	private long key = 0;
+	
+	public long getKey() {
+		return key;
+	}
+	
+	//64 bit
+	//Not sure if the key | "value" code returns expected result
+	public long convertToLong(String sequence) {
+		sequence = sequence.toLowerCase();
+		
+		for (int i = 0; i < sequence.length(); i++) {
+			if (sequence.charAt(i) == 'a') { //00
+				if (i == 0) {
+					key = 0;
+				}
+				else {
+					key = key << 2; //shift left by 2, fill with 0
+				}
+			}
+			if (sequence.charAt(i) == 't') { //11
+				if (i == 0) {
+					key = 1;
+				}
+				else {
+					key = key << 2; //shift left by 2
+					key = key | 1;
+					// key = key << 1;
+					// key + 1;
+					// key = key << 1;
+					// key + 1;
+				}
+			}
+			if (sequence.charAt(i) == 'c') { //01
+				if (i == 0) {
+					key = 2;
+				}
+				else {
+					key = key << 2; //shift left by 2
+					key = key | 2;
+					//key = key << 1;
+					//key + 0;
+					//key = key << 1;
+					//key + 1;
+				}
+			}
+			if (sequence.charAt(i) == 'g') { //10
+				if (i == 0) {
+					key = 3;
+				}
+				else {
+					key = key << 2; //shift left by 2
+					key = key | 3;
+					//key = key <<1;
+					//key = key + 1;
+					//key = key <<1; //fills with 0
+				}
+			}
+		}
+		return key;
+	}
+	
+	//31 length sequence
+	public String convertToString(long sequence, int length) {
+		String textSequence = "";
+		long temp = 0;
+		
+		for (int i = 1; i <= length; i++) {
+			//find starting point in bit sequence
+			temp = (sequence & 3L << (length - i) * 2);
+			//isolate sequence to two bits for comparison
+			temp = temp >> (length - i) * 2;
+				
+			//compare 
+			if (temp == 00) { 
+				textSequence += 'a';
+			}
+			else if (temp == 11) {
+				textSequence += 't';
+			}
+			else if (temp == 01) {
+				textSequence += 'c';
+			}
+			else if (temp == 10) {
+				textSequence += 'g';
+			}
+		}
+		return textSequence;
+	}
+}
