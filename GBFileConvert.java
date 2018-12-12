ed08b5c533334bca3cf0c39095e9c900cfb4b5fd
Bridgette Milgie <38337564+bmilgie@users.noreply.github.com>
2018-12-11 18:38:10 -0700

    Update GBFileConvert.java

--- GBFileConvert.java
+++ GBFileConvert.java
@@ -1,70 +1,100 @@
-import java.lang.Math;
 /**
  * Converts Long and Strings
  */
 public class GBFileConvert {
 	private long key = 0;
 
-	public long getKey() {
-		return key;
-	}
+    /**
+     * Converts a string type into a long, character by character a, c, g, t it
+     * converts to binary representation.
+     *
+     * @param dnaSubString substring of the dna sequence to convert into a long
+     * @return key which is a long type
+     */
+    long convertStringToLong(String dnaSubString)
+    {
+        dnaSubString = dnaSubString.toLowerCase();
 
-	//64 bit
-	/** my take on the string to long conversion
-	* converts letters in the string to numbers
-	* a = 0 c = 1 g = 2 t =3 (only letters it should be getting passed)
-	* then treats it as a base 4 number, ie:
-	* TAGCA = 3*4^4 + 0*4^3 + 2*4^2 + 1*4^1 + 0*4^0
-	**/
-	public long convertToLong(String sequence) {
-		sequence = sequence.toLowerCase();
-		long result = 0;
+        for (int i = 0; i < dnaSubString.length(); i++)
+        {
+            if (dnaSubString.charAt(i) == 'a')
+            { // 00
+                if (i == 0)
+                {
+                    key = 0;
+                } else
+                {
+                    key = key << 2;
+                }
+            }
+            if (dnaSubString.charAt(i) == 'c')
+            { // 01
+                if (i == 0)
+                {
+                    key = 1;
+                } else
+                {
+                    key = key << 2;
+                    key = key | 1;
+                }
+            }
+            if (dnaSubString.charAt(i) == 'g')
+            { // 10
+                if (i == 0)
+                {
+                    key = 2;
+                } else
+                {
+                    key = key << 2;
+                    key = key | 2;
+                }
+            }
+            if (dnaSubString.charAt(i) == 't')
+            { // 11
+                if (i == 0)
+                {
+                    key = 3;
+                } else
+                {
+                    key = key << 2;
+                    key = key | 3;
+                }
+            }
+        }
+        return key;
+    }
 
-		for(int i = 0; i < sequence.length(); i++){
-			long temp = 0;
-
-			if (sequence.charAt(i) == 'a') temp = 0;
-			if (sequence.charAt(i) == 'c') temp = 1;
-			if (sequence.charAt(i) == 'g') temp = 2;
-			if (sequence.charAt(i) == 't') temp = 3;
-			result += temp * Math.pow(4,sequence.length() - (1 + i));
-		}
-
-		return result;
-	}
-
-	//31 length sequence
-	/**
+    /**
      * Converts a long type into a String type
      *
-     * @param sequence    the long type to be converted
-     * @param length the length of each DNA sequence between splits
+     * @param dnaSequence    the long type to be converted
+     * @param sequenceLength the length of each DNA sequence between splits
      */
-	public String convertToString(long sequence, int length) {
-		String textSequence = "";
-		long temp = 0;
+    String convertLongToString(long dnaSequence, int sequenceLength)
+    {
+        StringBuilder str = new StringBuilder();
+        long temp;
 
-		for (int i = 1; i <= length; i++) {
-			//find starting point in bit sequence
-			temp = (sequence & 3L << (length - i) * 2);
-			//isolate sequence to two bits for comparison
-			temp = temp >> (length - i) * 2;
+        for (int i = 1; i <= sequenceLength; i++)
+        {
+            temp = (dnaSequence & (3L << ((sequenceLength - i) * 2)));
+            temp = temp >> ((sequenceLength - i) * 2);
 
-			//compare
-			if (temp == 0L) {
-				textSequence += 'a';
-			}
-			else if (temp == 1L) {
-				textSequence += 'c';
-			}
-			else if (temp == 2L) {
-				textSequence += 'g';
-			}
-			else if (temp == 3L) {
-				textSequence += 't';
-			}
-		}
-		return textSequence;
-	}
+            if (temp == 0L)
+            {
+                str.append("a");
+            } else if (temp == 1L)
+            {
+                str.append("c");
+            } else if (temp == 2L)
+            {
+                str.append("g");
+            } else if (temp == 3L)
+            {
+                str.append("t");
+            }
+        }
+        return str.toString();
+    }
 }
 
