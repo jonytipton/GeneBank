68775bdcf01fc3290983a096a223e5e95a70d539
Bridgette Milgie <38337564+bmilgie@users.noreply.github.com>
2018-12-11 18:37:29 -0700

    Update BTreeCacheNode.java

--- BTreeCacheNode.java
+++ BTreeCacheNode.java
@@ -1,103 +1,70 @@
 import java.util.Iterator;
 import java.util.LinkedList;
-
 /**
- * Creates BTreeCache node and maintains it through methods.
- */
+* Creates BTreeCache node and maintains it through methods
+*/
+public class BTreeCacheNode extends BTree
+{
+    BTreeCache bTreeCache;
 
-public class BTreeCacheNode extends BTree{
-	private BTreeNode data;
-	private int offset;
-	public BTreeCache BTC;
-	
-	public class BTreeCache implements Iterable<BTreeNode>{
-		
-		private final int MAX_SIZE;
-		private int numHits, numMisses;
-		
-		private LinkedList<BTreeNode> list;
-		
-		public BTreeCache(int MAX_SIZE){
-			this.MAX_SIZE = MAX_SIZE;
-			list = new LinkedList<BTreeNode>();
-		}
-		
-		public BTreeNode add(BTreeNode o, int offset){
-			BTreeNode rnode = null;
-			if(isFull()){
-				rnode = list.removeLast();
-			}
-			list.addFirst(o);
-			return rnode;
-		}
-		
-		public void clearCache(){
-			list.clear();
-		}
-		
-		public BTreeNode readNode(int offset){
-			for(BTreeNode n: list){
-				if(n.getOffset() == offset){
-					list.remove(n);
-					list.addFirst(n);
-					increaseHits();
-					return n;
-				}
-			}
-			increaseMiss();
-			return null;
-		}
-		
-		public int getNumReferences(){
-			return this.numHits + this.numMisses;
-		}
-		
-		private void increaseHits(){
-			this.numHits++;
-		}
-		
-		private void increaseMiss(){
-			this.numMisses++;
-		}
-		
-		public int getHits(){
-			return numHits;
-		}
-		
-		public int getNumMiss(){
-			return numMisses;
-		}
-		
-		public double getHitRatio(){
-			double ratio = ((double) getHits()) / getNumReferences();
-			return ratio;
-		}
-		
-		public int getSize(){
-			return list.size();
-		}
-		
-		public boolean isFull(){
-			return getSize() == this.MAX_SIZE;
-		}
-		
-		public Iterator<BTreeNode> iterator() {
-			return list.iterator();
-		}
-	}
-	
-	public BTreeCacheNode(BTreeNode data, int offset, int max){
-		this.data = data;
-		this.offset = offset;
-		this.BTC = new BTreeCache(max);
-	}
-	
-	public BTreeNode getData(){
-		return data;
-	}
-	
-	public int getOffset(){
-		return offset;
-	}
+    public BTreeCacheNode(int max)
+    {
+        this.bTreeCache = new BTreeCache(max);
+    }
 
+    public class BTreeCache implements Iterable<BTreeNode>
+    {
+
+        private final int MAX_SIZE;
+        private LinkedList<BTreeNode> list;
+
+        BTreeNode add(BTreeNode addNode)
+        {
+            BTreeNode tempNode = null;
+            if (isFull())
+            {
+                tempNode = list.removeLast();
+            }
+
+            list.addFirst(addNode);
+            return tempNode;
+        }
+
+        BTreeNode readNode(int offset)
+        {
+            for (BTreeNode readNode : list)
+            {
+                if (readNode.getOffset() == offset)
+                {
+                    list.remove(readNode);
+                    list.addFirst(readNode);
+                    return readNode;
+                }
+            }
+            return null;
+        }
+
+        BTreeCache(int MAX_SIZE)
+        {
+            this.MAX_SIZE = MAX_SIZE;
+
+            list = new LinkedList<>();
+        }
+
+        int getSize()
+        {
+            return list.size();
+        }
+
+        boolean isFull()
+        {
+            return getSize() == MAX_SIZE;
+        }
+
+        @Override
+        public Iterator<BTreeNode> iterator()
+        {
+            return list.iterator();
+        }
+    }
 }
